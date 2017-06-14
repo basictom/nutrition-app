b55dbb331f7972faeca6204ed87d90bdf0ab8b40
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-06-14 17:56:50 -0500

    working auth

--- javascripts/controllers/auth-ctrl.js
+++ javascripts/controllers/auth-ctrl.js
@@ -1,3 +1,41 @@
-app.controller("AuthCtrl", function(AuthFactory, FIREBASE_CONFIG){
-  console.log("auth ctrl");
+app.controller("AuthCtrl", function($location, $rootScope, $scope, AuthFactory, UserFactory){
+
+  $scope.auth = {};
+
+  let logMeIn = () => {
+    AuthFactory.authenticate($scope.auth).then((userCreds) => {
+      return UserFactory.getUser(userCreds.uid);
+    }, (error) => {
+      $scope.alerts.push({msg: error.message});
+      console.log("authenticate error", error);
+    }).then((user) => {
+      $rootScope.user = user;
+      // console.log("successful log in");
+      // $location.url("/board/view");
+    }).catch((error) => {
+      console.log("getUser error", error);
+    });
+  };
+
+  $scope.registerUser = () => {
+    console.log("register with user");
+    AuthFactory.registerWithEmail($scope.auth).then((didRegister) =>{
+      $scope.auth.uid = didRegister.uid;
+      return UserFactory.addUser($scope.auth);
+    }, (error) => {
+      console.log("registerWithEmail error", error);
+    })
+    .then((registerComplete) => {
+      logMeIn();
+    }).catch((error) => {
+      console.log("addUser error", error);
+    });
+  };
+
+  $scope.loginUser = () => {
+    logMeIn();
+  };
+
+
+
 });
